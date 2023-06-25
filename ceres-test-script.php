08357b49f700aeb571cc51f14bea35c6b7117470
patrickmj <p.murray-john@northeastern.edu>
2023-06-25 15:07:07 -0400

    gets basic alterations via GET param working

--- ceres-test-script.php
+++ ceres-test-script.php
@@ -4,7 +4,6 @@
  * Will be deleted eventually
  */
 
- echo "hello";
 
 
 /* Direct based on GET params */
@@ -12,43 +11,51 @@
 switch ($_GET['type']) {
     //yes I know $_GET is global, but I hate globals so I'm not going to pretend we're friends
     case 'html':
-        modifyHtml($_GET);
+        $html = modifyHtml($_GET);
     break;
 
     case 'list':
-        modifyList($_GET);
+        $html = modifyList($_GET);
     break;
 
     case 'map':
-        modifyMap($_GET);
+        $html = modifyMap($_GET);
     break;
 
     case 'storymap':
-        modifyStoryMap($_GET);
+        $html = modifyStoryMap($_GET);
     break;
 
-
+    default:
+        $html = "woot";
+    break;
 }
 
-function modifyHtml(array $params): string {
-    $htmlSpanReplaceHtml = "<span id='ceres-test-replace'>I need to go away</span>";
-    $htmlH2ReplaceHtml = "Hello! I'll be replaced someday!";
+echo $html;
 
-    if (isset($params['html'])) {
-        $htmlH2ReplaceHtml = $params['h2'];
+function modifyHtml(array $params): string {
+    $spanReplaceHtml = "<span id='ceres-test-replace'>I need to go away</span>";
+    $h2ReplaceHtml = "Hello! I'll be replaced someday!";
+
+    if (isset($params['h2'])) {
+        $h2ReplaceHtml = $params['h2'];
     }
 
     if (isset($params['span'])) {
-        $htmlSpanReplaceHtml = $params['span'];
+        $spanReplaceHtml = $params['span'];
+    }
+
+    if (isset($params['something'])) {
+        $somethingReplaceHtml = "whatevs";
     }
 
     $html = "<div id='ceres-container'>";
     $html .= "<h2>";
-    $html .= $htmlH2ReplaceHtml;
+    $html .= $h2ReplaceHtml;
     $html .= "</h2>";
 
     $html .= "<p>";
-    $html .= $htmlSpanReplaceHtml;
+    $html .= $spanReplaceHtml;
     $html .= "</p>";
 
     $html .= "</div>";
