c9c01656df72cb9d8326222a68e034d07c2ba7bc
patrickmj <p.murray-john@northeastern.edu>
2023-11-28 09:14:42 -0500

    mock shortcode responses

--- ceres-bb-modules.php
+++ ceres-bb-modules.php
@@ -8,30 +8,31 @@
  * Author: Aadesh Mallya, Patrick Murray-John, and the NU Digital Scholarship Group
  * Author URI: https://dsg.northeastern.edu/
  */
-define( 'CERES_BB_MODULES_DIR', plugin_dir_path( __FILE__ ) );
-define( 'CERES_BB_MODULES_URL', plugins_url( '/', __FILE__ ) );
 
-function my_load_module_examples() {
-    if ( class_exists( 'FLBuilder' ) ) {
-        // Include your custom modules here.
-
-    }
-}
-add_action( 'init', 'ceres_load_bb_simple' );
-
-function ceres_load_bb_simple() {
-    if ( class_exists( 'FLBuilder' ) ) {
-        require_once 'CeresBbModules/ceres_shortcode/CeresBbShortcodeModule.php';
-        require_once 'CeresBbModules/ceres_drs/CeresBbDynamicTableModule.php';
-        require_once 'CeresBbModules/ceres_image/CeresBbImageModule.php';
-        require_once 'CeresBbModules/ceres_gallerySlider/CeresBbGallerySliderModule.php';
-        require_once 'CeresBbModules/ceres_mediaPlaylist/CeresMediaPlaylistModule.php';
-        require_once 'CeresBbModules/ceres_tileGallery/CeresTileGalleryModule.php';
-    }
-
-}
-
+use function Ceres\BeaverBuilder\Utility\getMockShortcodeResponse;
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+ 
+require_once( 'utility/mockDrstkShortcodes.php');
+
+define( 'CERES_BB_MODULES_DIR', plugin_dir_path( __FILE__ ) );
+define( 'CERES_BB_MODULES_URL', plugins_url( '/', __FILE__ ) );
+define( 'CERES_ROOT_DIR', dirname(__FILE__, 2) . '/drs-toolkit-wp-plugin/libraries/Ceres');
+
+add_action( 'init', 'ceres_load_bb_simple' );
+
+function ceres_load_bb_simple() {
+    if ( class_exists( 'FLBuilder' ) ) {
+        require_once( 'CeresBbModules/ceres_shortcode/CeresBbShortcodeModule.php' );
+        require_once( 'CeresBbModules/ceres_drs/CeresBbDynamicTableModule.php');
+        require_once( 'CeresBbModules/ceres_image/CeresBbImageModule.php');
+        require_once( 'CeresBbModules/ceres_gallerySlider/CeresBbGallerySliderModule.php');
+        require_once( 'CeresBbModules/ceres_mediaPlaylist/CeresMediaPlaylistModule.php');
+        require_once( 'CeresBbModules/ceres_tileGallery/CeresTileGalleryModule.php');
+        
+    }
+}
+
+
